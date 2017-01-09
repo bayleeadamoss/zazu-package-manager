70950a0c5310e6b2ea101373b2445742f530704f
Blaine Schmeisser <blainesch@gmail.com>
2017-01-08 22:40:29 -0800

    Allow installing a theme.

--- src/installPlugin.js
+++ src/installPlugin.js
@@ -3,14 +3,25 @@ const fs = require('fs')
 const path = require('path')
 const zazuPath = path.join(os.homedir(), '.zazurc.json')
 const zazuConfig = require(zazuPath)
+const packages = require('./packages')
 
-module.exports = () => {
+module.exports = ({ cwd }) => {
   return (value, env = {}) => {
-    return new Promise((resolve, reject) => {
-      zazuConfig.plugins.push(value)
-      const zazuValue = JSON.stringify(zazuConfig, null, 2)
-      fs.writeFile(zazuPath, zazuValue, (err) => {
-        err ? reject(err) : resolve()
+    return packages.get(cwd).then((allPackages) => {
+      return allPackages.find((plugin) => {
+        return plugin.githuburl === value
+      })
+    }).then((plugin) => {
+      return new Promise((resolve, reject) => {
+        if (plugin.type === 'theme') {
+          zazuConfig.theme = plugin.githuburl
+        } else {
+          zazuConfig.plugins.push(plugin.githuburl)
+        }
+        const zazuValue = JSON.stringify(zazuConfig, null, 2)
+        fs.writeFile(zazuPath, zazuValue, (err) => {
+          err ? reject(err) : resolve()
+        })
       })
     })
   }
